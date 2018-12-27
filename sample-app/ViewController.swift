dfc28955988c4f52371bb120606d9b5b47adbddf
Takayuki Miyahara <voyager.3taka28@gmail.com>
2018-12-28 02:16:06 +0900

    Initial Commit

---
+++ sample-app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  sample-app
+//
+//  Created by Takayuki MIYAHARA on 2018/12/28.
+//  Copyright © 2018 Takayuki MIYAHARA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
